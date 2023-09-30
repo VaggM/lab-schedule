2420ee14ee60626bda4d20576292b808f4016305
vaggm <vagosmitikas@gmail.com>
2023-09-30 12:44:53 +0300

    connect script

--- scripts/open_app.sh
+++ scripts/open_app.sh
@@ -5,25 +5,32 @@ echo "program start"
 # app folder
 cd ~/lab-schedule
 
-# check_wifi_connection() {
-#   # Use 'nmcli' to check if there's an active Wi-Fi connection
-#   if nmcli -t -f GENERAL.STATE connection show --active | grep -q ":GENERAL.STATE:100"; then
-#     return 0
-#   else
-#     return 1
-#   fi
-# }
+check_wifi_connection() {
+  #!/bin/bash
 
-# # Check Wi-Fi connection
-# if check_wifi_connection; then
-#   echo "connected to Wi-Fi"
-#   git pull
-# else
-#   echo "not connected to Wi-Fi"
-# fi
+    echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
+
+    if [ $? -eq 0 ]; then
+        return 0
+    else
+        return 1
+    fi
+
+}
+
+# Check Wi-Fi connection
+if check_wifi_connection; then
+  echo "connected to Wi-Fi"
+  git pull
+else
+  echo "not connected to Wi-Fi"
+fi
 
 echo "starting node app"
 nohup node app.js > /dev/null 2>&1 &
+
+# Wait 5 seconds to start the app
+sleep 5
 
 echo "about to open firefox"
 firefox --kiosk http://localhost:8000/
